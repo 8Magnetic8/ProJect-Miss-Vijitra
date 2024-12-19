ccbb7a97f82debab369b2c7e94995a89151ed3e3
fffffam <sutthisakbuppha2545@gmail.com>
2024-12-19 17:28:02 +0700

    Add files via upload

---
+++ 661463004/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Midnew
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            showEvenNumber();
+            tableMultiplication();
+            //searchData();
+            //showEvenNumber1(1, 10);
+            searchData(10);
+            correct2(200000, 2);
+
+        }
+        public void showEvenNumber()
+        {
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Debug.WriteLine(i);
+            //    }
+            //}
+            //for (int i = 2; i <= 10; i=i+2)
+            //{
+            //    Debug.WriteLine(i);
+            //}
+            //for (int i = 1; i <= n; i=i+2)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Debug.WriteLine(i);
+            //    }
+            //}
+        }
+        //public void showEvenNumber1(int A, int N) 
+        //{
+        //    if (A % 2 == 1)  {
+        //        A++;
+        //    for (int i = A; i <= N; i = i + 2)
+        //    {   
+        //            Debug.WriteLine(i);
+        //    } 
+        //    }
+        //}
+        public void tableMultiplication()
+        {
+            for (int i = 0; i <= 12; i++)
+            {
+                Debug.WriteLine($"สูตรคูณแม่  {i} :");
+                for (int j = 0; j <= 12; j++) 
+                {
+                   Debug.WriteLine(i + "x" + j + "=" +(i * j));
+                }
+            }
+        }
+        public String searchData(int x)
+        {
+            int[] data = { 10, 20, 40, 10, 80, 30,90, 60 };
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == x)
+                { 
+                    Debug.WriteLine("Found");
+                    return "Found";
+                }
+            }
+            Debug.WriteLine("Not Found");
+            return "Not Found";
+        }
+        public bool correct2(int amount, int type)
+        {
+            if (amount <= 100000 || amount >= 400000)
+            {
+                amount = type * 100000;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ 661463004/Form2.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Midnew
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            //password();
+            //passwordNew();
+            //correct();
+            //correct1();
+            //correct2(200000,2);
+            Debug.WriteLine(Number1(90));
+        }
+        public void password()
+        {
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                for (char j = 'A'; j <= 'Z'; j++)
+                {
+                    for (char k = 'A'; k <= 'Z'; k++)
+                    {
+                        for (char l = 'A'; l <= 'Z'; l++)
+                        {
+                            Debug.WriteLine(i + j + k + l);
+                        }
+                    }
+                }
+            }
+        }
+        public void passwordNew()
+        {
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                for (char j = 'A'; j <= 'Z'; j++)
+                {
+                    for (char k = 'A'; k <= 'Z'; k++)
+                    {
+                        for (char l = 'A'; l <= 'Z'; l++)
+                        {
+                            Debug.WriteLine(i + j + k + l);
+                        }
+                    }
+                }
+            }
+        }
+        public bool correct(int amount,int type)
+        {
+            switch (type)
+            {
+                case 1: if (amount <= 100000) return true; break;
+                case 2: if (amount <= 200000) return true; break;
+                case 3: if (amount <= 300000) return true; break;
+                case 4: if (amount <= 400000) return true; break;
+            }
+            return false;
+        }
+        public bool correct1(int amount, int type)
+        {
+            int[] correct = { 100000, 200000, 300000, 400000 };
+            if(amount <= correct[type - 1])
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool correct2(int amount, int type)
+        {
+            if (amount <= (type * 100000))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private String Number1(int number)
+        {
+            //if(number >= 1 && number <= 9) return"กลุ่มที่1";
+            //if (number >= 10 && number <= 19) return "กลุ่มที่2";
+            //if (number >= 20 && number <= 29) return "กลุ่มที่3";
+            //if (number >= 30 && number <= 39) return "กลุ่มที่4";
+            //if (number >= 40 && number <= 49) return "กลุ่มที่5";
+            //if (number >= 50 && number <= 59) return "กลุ่มที่6";
+            //if (number >= 60 && number <= 69) return "กลุ่มที่7";
+            //if (number >= 70 && number <= 79) return "กลุ่มที่8";
+            //if (number >= 80 && number <= 89) return "กลุ่มที่9";
+            //if (number >= 90 && number <= 99) return "กลุ่มที่10";
+            int g = number / 10 + 1;            
+            return "กลุ่มที่: " + g;
+        }
+    }
+}
